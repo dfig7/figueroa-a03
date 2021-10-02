323556b7bd17a0ffe197f701ad787930e5fbf65e
diego <diegofigueroa0519@gmail.com>
2021-10-01 21:00:38 -0400

    exercise 25 almost done

--- exercise25/src/main/java/Solution25.java
+++ exercise25/src/main/java/Solution25.java
@@ -3,20 +3,30 @@
  *  Copyright 2021 Diego Figueroa
  */
 
+import java.util.Scanner;
+
 public class Solution25 {
+    private static final Scanner input = new Scanner(System.in);
     //create method to see if string has letters
     public static boolean lettersCheck(String a){
-        int len = a.length();
-        char[] charStringA = a.toCharArray();
-        if(a.)
+        if( a.matches("(?=.*[a-z]).*") ||  a.matches("(?=.*[A-Z]).*") )
+            return true;
+        else
+            return false;
     }
     //create method to see if string has numbers
     public static boolean numbersCheck(String a){
-
+        if( a.matches("(?=.*[0-9]).*") )
+            return true;
+        else
+            return false;
     }
     //create method to see if string has special characters
     public static boolean specialCheck(String a){
-
+        if( a.matches("(?=.*[~!@#$%^&*()_-]).*") )
+            return true;
+        else
+            return false;
     }
     //create a method that validates the password
     public static int passwordValidator(String a) {
@@ -27,18 +37,43 @@ public class Solution25 {
             fEightCharacters = 1;
 
         //Very Strong password has letters, numbers, special characters, and is at least eight characters
-
+        if(numbersCheck(a) && lettersCheck(a) && specialCheck(a) && fEightCharacters==1){
+            return 4;
+        }
         //Strong password has letters, at least one number, and is at least eight characters
+        else if(numbersCheck(a) && lettersCheck(a) && fEightCharacters==1){
+            return 3;
+        }
         //Weak password has only letters and is fewer than eight characters
+        else if(lettersCheck(a) && fEightCharacters==1){
+            return 2;
+        }
         // A very weak password has only numbers and is fewer than eight characters
+        else if(numbersCheck(a) && fEightCharacters==0) {
+            return 1;
+        }
         //if none meet the rules, password is unknown strength
+        else{
+            return 0;
+        }
     }
     //main
     public static void main(String[] args) {
-
-
-        //use method to ask for password
-        //use method passwordValidator
-        //print result
+        String x = input.nextLine();
+        if(passwordValidator(x)==4){
+            System.out.printf("The password \'%s\' is a very strong password.", x);
+        }
+        else if(passwordValidator(x)==3){
+            System.out.printf("The password \'%s\' is a strong password.", x);
+        }
+        else if(passwordValidator(x)==2){
+            System.out.printf("The password \'%s\' is weak password.", x);
+        }
+        else if(passwordValidator(x)==1){
+            System.out.printf("The password \'%s\' is a very weak password.", x);
+        }
+        else if(passwordValidator(x)==0){
+            System.out.printf("The password \'%s\' is an unknown strength password.", x);
+        }
     }
 }
