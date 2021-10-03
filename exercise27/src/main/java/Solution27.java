1a54bb7b14cb6f3f1d646c154e4965785b6de9c0
diego <diegofigueroa0519@gmail.com>
2021-10-03 12:46:03 -0400

    ex27 pseudo)

---
+++ exercise27/src/main/java/Solution27.java
@@ -0,0 +1,49 @@
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Diego Figueroa
+ */
+
+import java.util.Scanner;
+
+public class Solution27 {
+    private static final Scanner input = new Scanner(System.in);
+
+    // souf "Enter the first name: "
+        //return user's first name
+
+    // souf "Enter last name: "
+        //return user's last name
+
+    // souf "Enter zip code: "
+        //return zip
+
+    // souf "Enter Employee ID: "
+        //return ID
+
+    // Validate methods
+        //validate first name
+            //if first name length < 2
+                //error
+
+        //validate last name
+            //if last name length < 2
+                //error
+
+        //validate employee id
+            //if id is not 2 letters, a dash, and then 4 numbers <AA-####>
+                //error
+
+        //validate zip
+            //if zip is not a five digit number
+                //error
+
+    //ValidateInput
+        //method validates through all the validation tests
+            //returns errors from tests
+        //if no errors
+            //souf "no errors"
+
+    //main
+        //one souf that prints validateInput
+}
