d6eb54d70a5444bba3bbe5e6820dc4c9c31434bd
diego <diegofigueroa0519@gmail.com>
2021-10-03 14:15:53 -0400

    exercise 28 Final

--- execise28/src/main/java/Solution28.java
+++ execise28/src/main/java/Solution28.java
@@ -1,4 +1,5 @@
 
+
 /*
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
  *  Copyright 2021 Diego Figueroa
@@ -9,16 +10,28 @@ import java.util.Scanner;
 public class Solution28 {
     private static final Scanner input = new Scanner(System.in);
 
-    //create public static int askNumbers() {
-    //create answer
-    //for i=0: i<5; i++
-        //use a loop to ask the user for five numbers and adds them together
-            //"Enter a number: "
-            //answer += numbers
-        //return added values
+    public static int askNumbers() {
+        //create answer
+        int answer = 0;
 
-    //main
-    //souf answer
+        //for i=0: i<5; i++
+        for(int i=0; i<5; i++) {
+            //use a loop to ask the user for five numbers and adds them together
+            System.out.printf("Enter a number: ");
+            int num = input.nextInt();
+            //answer += numbers
+            answer += num;
+        }
+        //return added values
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        //souf answer
+        int x = askNumbers();
+        System.out.printf("The total is %d.", x);
+    }
 }
+
 
 
---
+++ execise28/src/test/java/Solution28Test.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution28Test {
+
+    @Test
+    void askNumbers() {
+        Solution28 x = new Solution28();
+
+        int actual = x.askNumbers();
+    }
+}
