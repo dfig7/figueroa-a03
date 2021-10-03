2511af482cbd69ff25a7116d4cef1bfd6b8ee933
diego <diegofigueroa0519@gmail.com>
2021-10-03 14:05:33 -0400

    ex28 pseudo

---
+++ execise28/src/main/java/Solution28.java
@@ -0,0 +1,24 @@
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Diego Figueroa
+ */
+
+import java.util.Scanner;
+
+public class Solution28 {
+    private static final Scanner input = new Scanner(System.in);
+
+    //create public static int askNumbers() {
+    //create answer
+    //for i=0: i<5; i++
+        //use a loop to ask the user for five numbers and adds them together
+            //"Enter a number: "
+            //answer += numbers
+        //return added values
+
+    //main
+    //souf answer
+}
+
+
