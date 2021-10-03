c6601e79fe2e538b4370d7abb3a2fd35869e35a0
diego <diegofigueroa0519@gmail.com>
2021-10-03 15:10:21 -0400

    ex 29 Final

--- exercise29/src/main/java/Solution29.java
+++ exercise29/src/main/java/Solution29.java
@@ -10,15 +10,34 @@ public class Solution29 {
     private static final Scanner input = new Scanner(System.in);
 
     //this exercise is about catching exceptions
-    //in calcYears method
-    //use try
-        //ask souf: what the rate of return is and store in rate
-        //check if the number is 0 or not a number
-            //if not
-                //souf 72 formula: years = 72/rate
-            //if yes
-                //catch number and arithmetic exceptions
+    //in findYears method
+    public static void findYears() {
+        boolean f = true;
 
+
+        //use try
+        while(f) {
+            try {
+                //ask souf: what the rate of return is and store in rate
+                System.out.printf("What is the rate of return? ");
+                //check if the number is 0 or not a number
+                int x = Integer.parseInt(input.nextLine());
+                //souf 72 formula: years = 72/rate
+                int years = 72 / x;
+                System.out.printf("It will take %d years to double your initial investment.", years);
+                f = false;
+            }
+            //if not
+             catch(NumberFormatException | ArithmeticException e){
+                //catch number and arithmetic exceptions
+                 System.out.printf("Sorry. That's not a valid input.");
+            }
+        }
+    }
     //main
+    public static void main(String[] args) {
+        findYears();
+    }
     //calcYears();
+
 }
---
+++ exercise29/src/test/java/Solution29Test.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution29Test {
+
+    @Test
+    void findYears() {
+        Solution29 x = new Solution29();
+
+
+    }
+}
