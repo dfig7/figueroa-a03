692933e76ac18b68153447ad62a9c018cb2cdab4
diego <diegofigueroa0519@gmail.com>
2021-10-03 11:52:39 -0400

    Solution 26 Pseudo

---
+++ exercise26/src/main/java/PaymentCalculator.java
@@ -0,0 +1,25 @@
+
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Diego Figueroa
+ */
+
+public class PaymentCalculator {
+    private static final Scanner input = new Scanner(System.in);
+
+    //Make method that asks for the balance <getBalance>
+
+    //Make method that asks for the APR on the card as a percent <getAPR>
+
+    //Make method that asks for the monthly payment <getMonthlyPayments
+
+    //Make public method <calculateMonthsUntilPaidOff> --> has no parameters and returns the number of months
+    //divide apr by 100 to turn into a decimal
+    //n = -(1/30) * log(1 + b/p * (1 - (1 + i)^30)) / log(1 + i)
+    //make sure to round numbers up by the cent
+
+}
+
---
+++ exercise26/src/main/java/Solution26.java
@@ -0,0 +1,19 @@
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Diego Figueroa
+ */
+
+import java.util.Scanner;
+
+public class Solution26 {
+    private static final Scanner input = new Scanner(System.in);
+
+    //make main method
+    public static void main(String[] args) {
+        //use payment calculator class
+        PaymentCalculator x = new PaymentCalculator();
+        //print the number of months in a souf rom the .calculateMonthsUntilPaidOff
+
+    }
+}
