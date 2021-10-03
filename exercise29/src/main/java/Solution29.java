6155cb91620efac78c99f712c3461e6290a86d33
diego <diegofigueroa0519@gmail.com>
2021-10-03 14:46:00 -0400

    Ex29 PseudoCode

---
+++ exercise29/src/main/java/Solution29.java
@@ -0,0 +1,24 @@
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Diego Figueroa
+ */
+
+import java.util.Scanner;
+
+public class Solution29 {
+    private static final Scanner input = new Scanner(System.in);
+
+    //this exercise is about catching exceptions
+    //in calcYears method
+    //use try
+        //ask souf: what the rate of return is and store in rate
+        //check if the number is 0 or not a number
+            //if not
+                //souf 72 formula: years = 72/rate
+            //if yes
+                //catch number and arithmetic exceptions
+
+    //main
+    //calcYears();
+}
