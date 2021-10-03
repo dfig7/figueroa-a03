3cd3626293b84abceba0d76c5e290cb4557e1552
diego <diegofigueroa0519@gmail.com>
2021-10-03 17:39:39 -0400

    ex31 Final

--- exercise31/src/main/java/Solution31.java
+++ exercise31/src/main/java/Solution31.java
@@ -10,30 +10,55 @@ public class Solution31 {
     private static final Scanner input = new Scanner(System.in);
 
     //getRestingPulse method
-    public static int getRestingulse() {
+    public static int getRestingPulse() {
 
         //make sure to have exception like question 29
 
-        while(true){
-            try{
-                System.out.printf("Resting Pulse: ");
+        while (true) {
+            try {
+                System.out.print("Resting Pulse: ");
                 int restingPulse = Integer.parseInt(input.nextLine());
                 return restingPulse;
-            }
-            catch(NumberFormatException a){
-                System.out.printf("Invalid input.");
+            } catch (NumberFormatException a) {
+                System.out.printf("Invalid input.\n");
             }
         }
     }
 
     //getAge method
+    //make sure to have exception like question 29
+    public static int getAge() {
+
         //make sure to have exception like question 29
 
+        while (true) {
+            try {
+                System.out.printf("Age: ");
+                int age = Integer.parseInt(input.nextLine());
+                return age;
+            } catch (NumberFormatException a) {
+                System.out.printf("Invalid input.\n");
+            }
+        }
+    }
+
     //createUI method with (restingPulse, Age) as parameters
+    public static void createUI(int restingPulse, int age) {
+
         //souf for category seperation
+        System.out.printf("\nIntensity\t| Rate\n----------|------------");
         //for i==55; i<= 95; i +=5
-        //souf print intensity and rate
+        for (int i = 55; i <= 95; i += 5) {
+            //TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+            int targetPulse = (int)(((220 - age) - restingPulse) * (i / 100.0) + restingPulse);
+            //souf print intensity and targetPulse
+            System.out.printf("\n%d%%\t\t  | %d bpm\n", i, targetPulse);
+        }
+    }
 
     //main
+    public static void main(String[] args) {
         //creeate GUI with method
+        createUI(getRestingPulse(), getAge());
+    }
 }
---
+++ exercise31/src/test/java/Solution31Test.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution31Test {
+
+    @Test
+    void getRestingpulseFalse() {
+        Solution31 x = new Solution31();
+        //can you do a test for this?
+
+    }
+
+    @Test
+    void getAgeFalse() {
+
+    }
+}
