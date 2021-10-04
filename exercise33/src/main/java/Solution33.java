54b15afd8bf0de36dcedd5965523edefe9249464
diego <diegofigueroa0519@gmail.com>
2021-10-03 20:34:18 -0400

    ex33 pseudo

---
+++ exercise33/src/main/java/Solution33.java
@@ -0,0 +1,20 @@
+
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Diego Figueroa
+ */
+
+import java.util.Scanner;
+
+public class Solution33 {
+    private static final Scanner input = new Scanner(System.in);
+    //magicBall method
+    //create an array with all possible strings that ball can say
+    //ask user for their question
+    //from 0 to array max length, randomly choose an answer from array
+        //souf
+
+    //main
+    //call magicBall
+}
