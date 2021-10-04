aebebc69af6c53d657773366ec4b2467f64a0dcf
diego <diegofigueroa0519@gmail.com>
2021-10-03 23:12:41 -0400

    ex35 Final

--- exercise35/src/main/java/Solution35.java
+++ exercise35/src/main/java/Solution35.java
@@ -5,16 +5,36 @@
  */
 
 
-public class Solution35 {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Solution35 {
+    private static final Scanner input = new Scanner(System.in);
     //create method that getsUserName and stores it in arrayList
-    //Have the method be a while loop that adds the names to list until list.length is 0
-        //souf "Enter name: "
+    public static ArrayList<String> getUserNames() {
+        ArrayList<String> list = new ArrayList<String>();
+        //Have the method be a while loop that adds the names to list until list.length is 0
+        while(true){
+            //souf "Enter name: "
+            System.out.printf("Enter a name: ");
+            String name = input.nextLine();
+            if(name.length()==0){
+                return list;
+            }
+            list.add(name);
+        }
+    }
 
     //create randomGenerator (ArrayList list)
+    public static void randomGenerator(ArrayList<String> list) {
         //chooses a random name from given parameter
-
+        int random = (int)(Math.random() * (list.size()+1));
+        System.out.printf("The winner is... %s", list.get(random));
+    }
     //main
+    public static void main(String[] args) {
+        randomGenerator(getUserNames());
+    }
     //randomGenerator(list)
 
 }
