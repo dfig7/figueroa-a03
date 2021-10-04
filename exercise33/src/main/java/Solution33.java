e8b2e7a66a3752b8abce143d4d2b2c2b0204256a
diego <diegofigueroa0519@gmail.com>
2021-10-03 21:14:40 -0400

    ex33 FINAL

--- exercise33/src/main/java/Solution33.java
+++ exercise33/src/main/java/Solution33.java
@@ -10,11 +10,19 @@ import java.util.Scanner;
 public class Solution33 {
     private static final Scanner input = new Scanner(System.in);
     //magicBall method
-    //create an array with all possible strings that ball can say
-    //ask user for their question
-    //from 0 to array max length, randomly choose an answer from array
+    public static void magicBall() {
+        //create an array with all possible strings that ball can say
+        String[] ball = {"Yes", "No", "Maybe", "Ask again later."};
+        //ask user for their question
+        System.out.printf("What's your question?\n> ");
+        input.nextLine();
+        //from 0 to array max length, randomly choose an answer from array
+        System.out.printf("\n%s", ball[(int)(Math.random() * (ball.length+1) + 0)]);
         //souf
-
+    }
     //main
-    //call magicBall
+    public static void main(String[] args) {
+        magicBall();
+        //call magicBall
+    }
 }
