222c9881e51c214d01537f10b0a1af1293704213
diego <diegofigueroa0519@gmail.com>
2021-10-03 22:03:24 -0400

    ex34 pseudo

---
+++ exercise34/src/main/java/Solution34.java
@@ -0,0 +1,29 @@
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Diego Figueroa
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution34 {
+    private static final Scanner input = new Scanner(System.in);
+
+    //create a method that creates a list of 5 names
+    public static ArrayList<String> employeeList() {
+        //use .add to add string to array
+        ArrayList<String> list = new ArrayList<String>();
+
+    }
+    //make a method that prints all 5 employees
+        //for loop (String employee: list)
+
+    //create a method that removes an employee by the name input
+        //use .remove
+
+    //main
+        //create an employeeList
+        //printEmployees
+        //removeEmployees
+}
