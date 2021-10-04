4d367112e7c4156c1e79e1577a51736e51075b51
diego <diegofigueroa0519@gmail.com>
2021-10-03 22:57:04 -0400

    ex35 pseudo

---
+++ exercise35/src/main/java/Solution35.java
@@ -0,0 +1,20 @@
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Diego Figueroa
+ */
+
+
+public class Solution35 {
+
+    //create method that getsUserName and stores it in arrayList
+    //Have the method be a while loop that adds the names to list until list.length is 0
+        //souf "Enter name: "
+
+    //create randomGenerator (ArrayList list)
+        //chooses a random name from given parameter
+
+    //main
+    //randomGenerator(list)
+
+}
